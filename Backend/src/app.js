54f9c5b5ae053585cb1a21bc868cc79e3c400c2e
Ashutosh Kumar <ashutoshashu882@gmail.com>
2025-08-29 00:38:51 +0530

    .

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,12 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", (process.env.PORT || 8000));
-app.use(cors());
+
+app.use(cors({
+  origin: "https://hola-hub-1.onrender.com",   // frontend
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
 app.use(express.json({"limit": "40kb"}));
 app.use(express.urlencoded({"limit": "40kb", extended: true}));
 
